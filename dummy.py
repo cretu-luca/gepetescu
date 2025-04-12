9fd3a25a9654d6c869a519a9b55411b0d9394653
luca <lucac375@gmail.com>
2025-04-12 20:16:51 +0300

    dummy arch

--- dummy.py
+++ dummy.py
@@ -34,7 +34,7 @@ class DummyGPTModel(nn.Module):
     def forward(self, in_idx):
         batch_size, seq_len = in_idx.shape
         tok_embeds = self.tok_emb(in_idx)
-        pos_embeds = self.pos_emb(
+        pos_embeds = self.pos_emb( 
             torch.arange(seq_len, device=in_idx.device)
         )
 
@@ -147,3 +147,56 @@ ffn = FeedForward(GPT_CONFIG_124M)
 x = torch.rand(2, 3, 768)
 out = ffn(x)
 print(out.shape)
+
+class ExampleDeepNeuralNetwork(nn.Module):
+    def __init__(self, layer_sizes, use_shortcut):
+        super().__init__()
+        self.use_shortcut = use_shortcut
+        self.layers = nn.ModuleList([
+            nn.Sequential(nn.Linear(layer_sizes[0], layer_sizes[1]),
+                GELU()),
+                nn.Sequential(nn.Linear(layer_sizes[1], layer_sizes[2]),
+                GELU()),
+                nn.Sequential(nn.Linear(layer_sizes[2], layer_sizes[3]),
+                GELU()),
+                nn.Sequential(nn.Linear(layer_sizes[3], layer_sizes[4]),
+                GELU()),
+                nn.Sequential(nn.Linear(layer_sizes[4], layer_sizes[5]),
+                GELU())
+            ])
+        
+    def forward(self, x):
+        for layer in self.layers:
+            layer_output = layer(x)
+            if self.use_shortcut and x.shape == layer_output.shape:
+                x = x + layer_output
+            else:
+                x = layer_output
+            
+        return x
+        
+layer_sizes = [3, 3, 3, 3, 3, 1]
+sample_input = torch.tensor([[1., 0., -1.]])
+torch.manual_seed(123)
+model_without_shortcut = ExampleDeepNeuralNetwork(layer_sizes, use_shortcut=False)
+
+def print_gradients(model, x):
+    output = model(x)
+    target = torch.tensor([[0.]])
+    loss = nn.MSELoss()
+    loss = loss(output, target)
+
+    loss.backward()
+
+    for name, param in model.named_parameters():
+        if 'weight' in name:
+            print(f"{name} has gradient mean of {param.grad.abs().mean().item()}")
+
+
+print_gradients(model_without_shortcut, sample_input)
+
+torch.manual_seed(123)
+model_with_shortcut = ExampleDeepNeuralNetwork(
+layer_sizes, use_shortcut=True
+)
+print_gradients(model_with_shortcut, sample_input)
