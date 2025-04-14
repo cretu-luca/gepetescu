f4a904361c55d08d482ea420de796f1b107ecc5a
luca <lucac375@gmail.com>
2025-04-14 13:18:48 +0300

    refactor

--- multihead_attention.py
+++ multihead_attention.py
@@ -60,27 +60,3 @@ class MultiHeadAttention(nn.Module):
 
         context_vec = self.out_proj(context_vec)
         return context_vec
-
-inputs = torch.tensor([
-    [0.43, 0.15, 0.89], 
-    [0.55, 0.87, 0.66],
-    [0.57, 0.85, 0.64], 
-    [0.22, 0.58, 0.33], 
-    [0.77, 0.25, 0.10], 
-    [0.05, 0.80, 0.55]
-])
-
-d_in = inputs.shape[1]
-d_out = 2
-
-batch = torch.stack((inputs, inputs), dim=0)
-print(batch.shape)
-
-
-torch.manual_seed(123)
-batch_size, context_length, d_in = batch.shape
-d_out = 2
-mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
-context_vecs = mha(batch)
-print(context_vecs)
-print("context_vecs.shape:", context_vecs.shape)
--- transformer_utils.py
+++ transformer_utils.py
@@ -12,12 +12,12 @@ class GELU(nn.Module):
         ))
     
 class FeedForward(nn.Module):
-    def __init__(self, cfg):
+    def __init__(self, config):
         super().__init__()
         self.layers = nn.Sequential(
-            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]), 
+            nn.Linear(config["emb_dim"], 4 * config["emb_dim"]), 
             GELU(),
-            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"])
+            nn.Linear(4 * config["emb_dim"], config["emb_dim"])
         )
 
     def forward(self, x):
