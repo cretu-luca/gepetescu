a7c68da86dfe60ddd1b181b93b0e90b143e238fc
luca <lucac375@gmail.com>
2025-04-17 21:24:39 +0300

    top-k sampling with temperature scaling

--- training.py
+++ training.py
@@ -184,10 +184,46 @@ train_losses, val_losses, tokens_seen = train_model_simple(
     start_context="Every effort moves you", tokenizer=tokenizer
 )
 
-token_ids = generate_text_simple(
+def generate(model, idx, max_new_tokens, context_size, 
+             temperature=0.0, top_k=None, eos_id=None):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[:, -1, :]
+
+        if top_k is not None:
+            top_logits = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(
+                condition=logits < min_val,
+                input=torch.tensor(float('-inf')).to(logits.device),
+                other=logits
+            )
+
+        if temperature > 0.0:
+            logits = logits / temperature
+            probs = torch.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=-1)
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+
+        if idx_next == eos_id:
+            break
+
+        idx = torch.cat((idx, idx_next), dim=-1)
+
+    return idx
+
+token_ids = generate(
     model=model,
     idx=text_to_token_ids("Every effort moves you", tokenizer),
-    max_new_tokens=25,
-    context_size=config["context_length"]
+    max_new_tokens=15,
+    context_size=config["context_length"],
+    top_k=25,
+    temperature=1.4
 )
+
 print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
